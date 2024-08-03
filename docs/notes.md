b7e5de929e1696896388d2d78a845b82634b524f
sgauravm <gauravsnh210@gmail.com>
2024-08-03 15:37:08 -0400

    finished todo app

---
+++ docs/notes.md
@@ -0,0 +1,12 @@
+## Styling
+
+- 4 ways of styling your app
+
+  - inline styling: can be provided as style property
+    - takes js object and property name are similar to css but in camelCase. eg font-size=> fontSize
+  - css style sheet: One file containing classes for entire application
+    - create class and import it
+  - css modules: having css modules for each component.
+  - css library like tailwind
+
+- Concept of taking up the state varibale and moving it to parent component is calles lifting stae variable
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,35 @@
+import styles from "./todoItem.module.css";
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+
+  const completedClass = item.done ? styles.completed : "";
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span className={completedClass} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
+}
